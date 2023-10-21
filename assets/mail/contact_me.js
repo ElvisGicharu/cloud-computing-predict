27599f86f94619a6cb13b7875a71aa6961b4a965
Elvis Gicharu <gicharuelvis@gmail.com>
2023-10-21 17:40:52 +0300

    updates

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://b53pb2m7fe.execute-api.eu-west-1.amazonaws.com/portfolio-lambda",
+                url: "https://j2orlgegmf.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
